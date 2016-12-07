04a795e4bbb3c2aed1303eb02f8866f7df006f9c
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-07 17:12:12 +0100

    prepublish

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = equalStreamContents;
 
+var _streamToPromise = require('stream-to-promise');
+
+var _streamToPromise2 = _interopRequireDefault(_streamToPromise);
+
 var _gulpCached = require('gulp-cached');
 
 var _gulpCached2 = _interopRequireDefault(_gulpCached);
@@ -18,16 +22,11 @@ function equalStreamContents(stream1, stream2) {
   var cacheName1 = cacheName + 1;
   var cacheName2 = cacheName + 2;
 
-  var p1 = new Promise(function (resolve, reject) {
-    stream1.pipe((0, _gulpCached2.default)(cacheName1)).on('finish', function () {
-      resolve(_gulpCached2.default.caches[cacheName1]);
-    }).on('error', reject);
+  var p1 = (0, _streamToPromise2.default)(stream1.pipe((0, _gulpCached2.default)(cacheName1))).then(function () {
+    return _gulpCached2.default.caches[cacheName1];
   });
-
-  var p2 = new Promise(function (resolve, reject) {
-    stream2.pipe((0, _gulpCached2.default)(cacheName2)).on('finish', function () {
-      resolve(_gulpCached2.default.caches[cacheName2]);
-    }).on('error', reject);
+  var p2 = (0, _streamToPromise2.default)(stream2.pipe((0, _gulpCached2.default)(cacheName2))).then(function () {
+    return _gulpCached2.default.caches[cacheName2];
   });
 
   return Promise.all([p1, p2]).then(function (caches) {
