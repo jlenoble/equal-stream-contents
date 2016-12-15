723a0aa21dc4378f8ce79d509871ac07dae83d32
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-15 23:33:31 +0100

    Removed static test files
    Now are generated dynamically, then deleted

--- test/equal-stream-contents.test.js
+++ test/equal-stream-contents.test.js
@@ -1,6 +1,9 @@
 import gulp from 'gulp';
 import {expect} from 'chai';
 import equalStreamContents from '../src/equal-stream-contents';
+import {tmpDir} from 'cleanup-wrapper';
+import touch from 'touch';
+import mkdirp from 'mkdirp'
 
 describe('Testing equalStreamContents', function() {
 
@@ -14,7 +17,11 @@ describe('Testing equalStreamContents', function() {
     function() {
       return equalStreamContents(gulp.src('gulp/**/*.js'),
         gulp.src(['gulp/**/*.js', '!gulp/globs.js'])).catch(err => {
-          expect(err.toString()).to.match(/AssertionError: expected \{ Object.* to deeply equal \{ Object/);
+          try {
+            expect(err.toString()).to.match(/AssertionError: expected \{ Object.* to deeply equal \{ Object/);
+          } catch (e) {
+            throw err;
+          }
         });
     });
 
@@ -23,17 +30,28 @@ describe('Testing equalStreamContents', function() {
 describe('Testing equalStreamContents with more than 16 files', function() {
 
   it(`equalStreamContents returns a promise that resolves on equality`,
-    function() {
-      return equalStreamContents(gulp.src('test/files/*'),
-        gulp.src('test/files/*'));
-    });
+    tmpDir('tmp', function() {
+      mkdirp.sync('tmp');
+      for (let i = 0; i < 20; i++) {
+        touch.sync('tmp/a' + i);
+      }
+      return equalStreamContents(gulp.src('tmp/*'), gulp.src('tmp/*'));
+    }));
 
   it(`equalStreamContents returns a promise that rejects on inequality`,
-    function() {
-      return equalStreamContents(gulp.src('test/files/*'),
-        gulp.src(['test/files/*', '!test/files/z'])).catch(err => {
-          expect(err.toString()).to.match(/AssertionError: expected \{ Object.* to deeply equal \{ Object/);
+    tmpDir('tmp', function() {
+      mkdirp.sync('tmp');
+      for (let i = 0; i < 20; i++) {
+        touch.sync('tmp/a' + i);
+      }
+      return equalStreamContents(gulp.src('tmp/*'),
+        gulp.src(['tmp/*', '!tmp/a0'])).catch(err => {
+          try {
+            expect(err.toString()).to.match(/AssertionError: expected \{ Object.* to deeply equal \{ Object/);
+          } catch (e) {
+            throw err;
+          }
         });
-    });
+    }));
 
 });
