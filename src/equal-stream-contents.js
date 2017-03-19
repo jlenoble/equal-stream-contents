389590f20ed9b96f40efca6dce0823de3cb986a8
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-20 00:39:12 +0100

    Changed cache naming scheme
    Now use a counter, not current time. Process may be too fast and
    some name collisions could happen.

--- src/equal-stream-contents.js
+++ src/equal-stream-contents.js
@@ -2,14 +2,16 @@ import streamToPromise from 'stream-to-promise';
 import cached from 'gulp-cached';
 import {expect} from 'chai';
 
-export default function equalStreamContents(stream1, stream2) {
-  const cacheName = '__CACHE_' + (new Date()).getTime() + '_';
+let counter = 0;
+
+export default function equalStreamContents (stream1, stream2) {
+  const cacheName = '__CACHE_' + (counter++) + '_';
   const cacheName1 = cacheName + 1;
   const cacheName2 = cacheName + 2;
 
-  var p1 = streamToPromise(stream1.pipe(cached(cacheName1)))
+  const p1 = streamToPromise(stream1.pipe(cached(cacheName1)))
     .then(() => cached.caches[cacheName1]);
-  var p2 = streamToPromise(stream2.pipe(cached(cacheName2)))
+  const p2 = streamToPromise(stream2.pipe(cached(cacheName2)))
     .then(() => cached.caches[cacheName2]);
 
   return Promise.all([p1, p2])
