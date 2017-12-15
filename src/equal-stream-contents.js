1223d3bd94530895bcc46e7585d353cd4aca06aa
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-15 02:57:32 +0100

    More error handling
    Always clear caches on start in case they are leftover
    from an uncaught earlier error within an arg stream.
    
    Also name caches more specifically.
    
    Also error if potential memory leak.

--- src/equal-stream-contents.js
+++ src/equal-stream-contents.js
@@ -5,25 +5,64 @@ import {expect} from 'chai';
 let counter = 0;
 
 export default function equalStreamContents (stream1, stream2) {
-  const cacheName = '__CACHE_' + (counter++) + '_';
+  const cacheName = '__CACHE_ESC' + (counter++) + '_';
   const cacheName1 = cacheName + 1;
   const cacheName2 = cacheName + 2;
 
+  const clearCaches = () => {
+    if (cached.caches[cacheName1]) {
+      delete cached.caches[cacheName1];
+    }
+    if (cached.caches[cacheName2]) {
+      delete cached.caches[cacheName2];
+    }
+  };
+
+  const cacheNames = Object.keys(cached.caches);
+  const nCaches = cacheNames.length;
+
+  if (cached.caches[cacheName1] || cached.caches[cacheName2]) {
+    error({
+      message: 'Caches already exist',
+      explain: [
+        ['Caches are one or both of:', [cacheName1, cacheName2]],
+        'You probably run concurrently different versions',
+        'of equalStreamContents',
+      ],
+    });
+  }
+
+  if (nCaches > 30 && !equalStreamContents.noCacheLimit) {
+    error({
+      message: 'Too many gulp-cached caches',
+      explain: [
+        ['Number of current caches:', nCaches],
+        ['Names:', cacheNames],
+        ['If you are fine with that, add in your code:',
+          'equalStreamContents.noCacheLimit = true'],
+        'You may also want to check this package is properly deduped',
+      ],
+    });
+  }
+
+  const clearCachesAndThrow = err => {
+    clearCaches();
+    throw err;
+  };
+
   const p1 = streamToPromise(stream1.pipe(cached(cacheName1)))
-    .then(() => cached.caches[cacheName1]);
+    .then(() => cached.caches[cacheName1], clearCachesAndThrow);
   const p2 = streamToPromise(stream2.pipe(cached(cacheName2)))
-    .then(() => cached.caches[cacheName2]);
+    .then(() => cached.caches[cacheName2], clearCachesAndThrow);
+
 
   return Promise.all([p1, p2])
     .then(caches => {
       try {
         expect(caches[0]).to.eql(caches[1]);
-        delete cached.caches[cacheName1];
-        delete cached.caches[cacheName2];
+        clearCaches();
       } catch (e) {
-        delete cached.caches[cacheName1];
-        delete cached.caches[cacheName2];
-        throw e;
+        clearCachesAndThrow(e);
       }
     });
 };
